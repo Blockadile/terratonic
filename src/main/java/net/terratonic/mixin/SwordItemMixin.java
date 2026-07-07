9fb1514544ea8a8a40123fcfc9fb1d6af2938ed5
Blockadile <blockadile07@gmail.com>
2026-07-07 03:32:45 -0700

    Commit #13 --------------- Sword Item Mixin Example Mixin removal

---
+++ src/main/java/net/terratonic/mixin/SwordItemMixin.java
@@ -0,0 +1,28 @@
+package net.terratonic.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SwordItem.class)
+public class SwordItemMixin {
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BLOCK;
+    }
+
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 72000;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        user.setCurrentHand(hand);
+        return TypedActionResult.consume(itemStack);
+    }
+}
